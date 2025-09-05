0a26c312326e0bd6e52ecb8ecf87e4d80c40acb6
Sandhavi Wanigasooriya <sandhaviwanigasooriya@gmail.com>
2025-09-05 20:12:54 +0530

    feat: Add Google Services plugin configuration to build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,3 +1,14 @@
+// Add the Google Services plugin to the classpath
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.google.gms:google-services:4.4.0")
+    }
+}
+
 allprojects {
     repositories {
         google()
